b5bbc7140de6815232884cdf64fc59e45e4cfef0
hirohiro.tornade <hirohiro.tornade@3035c6ce-779e-11de-91f8-ab615253d9e8>
2009-08-04 14:02:19 +0000

    amidakuji初回インポート

---
+++ GoogleAppEngine20090808/amidakuji/src/com/kyotogtug/amidakuji/AmidakujiServlet.java
@@ -0,0 +1,13 @@
+package com.kyotogtug.amidakuji;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class AmidakujiServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
