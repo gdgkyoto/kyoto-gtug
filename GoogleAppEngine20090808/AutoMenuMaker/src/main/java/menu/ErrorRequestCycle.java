2476ed6669819b0171310f55e68478c5d8fa41b1
Kitamura.Takuya <Kitamura.Takuya@3035c6ce-779e-11de-91f8-ab615253d9e8>
2009-08-05 20:21:35 +0000

    WicketApplicationをAutoMenuMakerApplicationに名称変更します。 また、エラーページのカスタマイズをおこないました。

---
+++ GoogleAppEngine20090808/AutoMenuMaker/src/main/java/menu/ErrorRequestCycle.java
@@ -0,0 +1,24 @@
+package menu;
+
+import menu.page.ErrorPage;
+
+import org.apache.wicket.Page;
+import org.apache.wicket.Response;
+import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.protocol.http.WebRequest;
+import org.apache.wicket.protocol.http.WebRequestCycle;
+
+public class ErrorRequestCycle extends WebRequestCycle {
+
+	@Override
+	public Page onRuntimeException(Page page, RuntimeException e) {
+
+		return new ErrorPage(e);
+
+	}
+
+	public ErrorRequestCycle(WebApplication application, WebRequest request, Response response) {
+		super(application, request, response);
+	}
+
+}
