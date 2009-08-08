f385bf0df357e2442edf8fb95d102faf3e9c9552
gilpa80 <gilpa80@3035c6ce-779e-11de-91f8-ab615253d9e8>
2009-08-08 01:03:03 +0000

    sample　修正

--- GoogleAppEngine20090808/JRuby-team/WEB-INF/app.rb
+++ GoogleAppEngine20090808/JRuby-team/WEB-INF/app.rb
@@ -2,18 +2,31 @@ require 'rubygems'
 require 'sinatra'
 require 'bumble'
 require 'time'
+include_class 'com.google.api.translate.Language';
+include_class 'com.google.api.translate.Translate';
 
-class Person
+
+class Trans
 	include Bumble
-	ds :content
+	ds :original, :jpToEn, :enToKo, :koToEn, :enToJp, :create_at
 end
 
 get '/' do
-	@persons = Person.all
+	@trans = Trans.all
 	erb :index
 end
 
 post '/' do
-	Person.create(params['person'])
+	Translate.http_referrer = "http://code.google.com/p/google-api-translate-java/"
+	jpToEn = Translate.translate(params[:trans][:original],Language::JAPANESE, Language::ENGLISH);
+	enToKo = Translate.translate(jpToEn,Language::ENGLISH, Language::KOREAN);
+	koToEn = Translate.translate(enToKo,Language::KOREAN, Language::ENGLISH);
+	enToJp = Translate.translate(koToEn,Language::ENGLISH, Language::JAPANESE);
+	params[:trans][:jpToEn]=jpToEn
+	params[:trans][:enToKo]=enToKo
+	params[:trans][:koToEn]=koToEn
+	params[:trans][:enToJp]=enToJp
+	params[:trans][:create_at]=Time.now
+	Trans.create(params['trans'])
 	redirect '/'
 end
