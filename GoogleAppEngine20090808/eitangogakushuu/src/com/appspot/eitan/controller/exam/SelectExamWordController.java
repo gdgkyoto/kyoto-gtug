bb82ceb985972de3e7a1c56fb8cf7fdec1879441
masaki.toyoshima <masaki.toyoshima@3035c6ce-779e-11de-91f8-ab615253d9e8>
2009-08-08 07:23:28 +0000

    再度絞り込み条件追加

--- GoogleAppEngine20090808/eitangogakushuu/src/com/appspot/eitan/controller/exam/SelectExamWordController.java
+++ GoogleAppEngine20090808/eitangogakushuu/src/com/appspot/eitan/controller/exam/SelectExamWordController.java
@@ -10,6 +10,10 @@ import org.slim3.controller.validator.Validators;
 
 import com.appspot.eitan.model.UserInfo;
 import com.appspot.eitan.model.WordInfo;
+import com.appspot.eitan.util.Filtering;
+import com.appspot.eitan.util.ListUtil;
+import com.appspot.eitan.util.SearchCountFiltering;
+import com.appspot.eitan.util.StatusFiltering;
 
 public class SelectExamWordController extends Controller {
 
@@ -27,16 +31,46 @@ public class SelectExamWordController extends Controller {
         UserInfo userInfo = (UserInfo) sessionScope("loginUser");
         List<WordInfo> wordList = userInfo.getWordList();
 
+        List<Filtering> filterList = new ArrayList<Filtering>();
+
+        Integer searchCount = asInteger("searchCount");
+        if (searchCount != null) {
+            filterList.add(new SearchCountFiltering(
+                userInfo.getKey(),
+                searchCount));
+        }
+
+        String[] parameterValues = request.getParameterValues("status");
+        if (parameterValues != null) {
+            int[] numAry = new int[parameterValues.length];
+            for (int i = 0; i < parameterValues.length; i++) {
+                numAry[i] = Integer.valueOf(parameterValues[i]);
+            }
+
+            filterList.add(new StatusFiltering(userInfo.getKey(), searchCount));
+        }
+
+        for (Filtering f : filterList) {
+            wordList = ListUtil.instance().filter(wordList, f);
+        }
+
+        Integer wordCount = asInteger("wordCount");
+        if (wordCount != null) {
+            if (wordList.size() > wordCount) {
+                wordList = wordList.subList(0, wordCount);
+            }
+        }
+
         // セッションに単語リストをセット
         sessionScope("wordList", wordList);
 
-        List<String> wordKeyList    = new ArrayList<String>();
-        for(int i = 0 ; i < wordList.size();i++){
+        List<String> wordKeyList = new ArrayList<String>();
+        for (int i = 0; i < wordList.size(); i++) {
             WordInfo wi = wordList.get(i);
-            String key  = wi.getKey();
+            String key = wi.getKey();
             wordKeyList.add(key);
         }
-        sessionScope("wordKeyList",wordKeyList);
+        sessionScope("wordKeyList", wordKeyList);
 
         // 各種情報をリクエストにセット
         int index = 1;
