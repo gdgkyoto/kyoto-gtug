845e723376c03c858b97d9b7f47be4aca3d95654
takashi.someda <takashi.someda@3035c6ce-779e-11de-91f8-ab615253d9e8>
2009-08-08 06:36:10 +0000

    request オブジェクトをグローバルからローカルスコープへ

--- GoogleAppEngine20090808/python-group/geo_tweet/src/js/gt_api.js
+++ GoogleAppEngine20090808/python-group/geo_tweet/src/js/gt_api.js
@@ -24,17 +24,19 @@ function post_message(requestObj, msg, place, callback) {
 	}
 	requestObj.onreadystatechange = callback;
 
-  requestObj.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded'); 
+	requestObj.setRequestHeader('Content-Type',
+			'application/x-www-form-urlencoded');
 	requestObj.send(data);
 }
 
 function get_messages(requestObj, area, callback) {
 	var path = '/messages.json';
-	
+
 	if (area) {
-		path += "?lat=" + area.center.latitude + "&lon=" + area.center.longitude + "&rad=" + area.radius;
+		path += "?lat=" + area.center.latitude + "&lon="
+				+ area.center.longitude + "&rad=" + area.radius;
 	}
-	
+
 	requestObj.open('GET', path);
 	callback.request = requestObj;
 	requestObj.onreadystatechange = callback;
@@ -42,24 +44,25 @@ function get_messages(requestObj, area, callback) {
 }
 
 function postData(request, path, data, callback) {
-  request.open('POST', path);
-  request.onreadystatechange = callback;
-  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded'); 
-  request.send(data);
+	request.open('POST', path);
+	request.onreadystatechange = callback;
+	request.setRequestHeader('Content-Type',
+			'application/x-www-form-urlencoded');
+	request.send(data);
 }
 
 function updateUserLocation(userId, lat, lon, callback) {
-  path = "/users/" + userId;
-  data = "lat=" + encodeURIComponent(lat) + "&lon=" + encodeURIComponent(lon);
-  request = getXMLHttpRequestObject();
-  postData(request, path, data, updateUserLocationResultHandler);
+	var path = "/users/" + userId;
+	var data = "lat=" + encodeURIComponent(lat) + "&lon=" + encodeURIComponent(lon);
+	var req = getXMLHttpRequestObject();
+	postData(req, path, data, updateUserLocationResultHandler);
 }
 
 function updateUserLocationResultHandler() {
-  if (theRequest.readyState == 4 && theRequest.status == 200) {
-    // Succeeded
-    alert("Succeeded");
-  } else {
-    //alert("Failed");
-  }
+//	if (theRequest.readyState == 4 && theRequest.status == 200) {
+//		// Succeeded
+//		alert("Succeeded");
+//	} else {
+//		// alert("Failed");
+//	}
 }
