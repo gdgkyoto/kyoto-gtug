12fe6f2ffec6e2e8372b9bf171911b3eb727bdf9
takashi.someda <takashi.someda@3035c6ce-779e-11de-91f8-ab615253d9e8>
2009-08-08 07:02:36 +0000

    メッセージ表示用の Overlay を追加

--- GoogleAppEngine20090808/python-group/geo_tweet/src/js/gt_api.js
+++ GoogleAppEngine20090808/python-group/geo_tweet/src/js/gt_api.js
@@ -53,16 +53,64 @@ function postData(request, path, data, callback) {
 
 function updateUserLocation(userId, lat, lon, callback) {
 	var path = "/users/" + userId;
-	var data = "lat=" + encodeURIComponent(lat) + "&lon=" + encodeURIComponent(lon);
+	var data = "lat=" + encodeURIComponent(lat) + "&lon="
+			+ encodeURIComponent(lon);
 	var req = getXMLHttpRequestObject();
 	postData(req, path, data, updateUserLocationResultHandler);
 }
 
 function updateUserLocationResultHandler() {
-//	if (theRequest.readyState == 4 && theRequest.status == 200) {
-//		// Succeeded
-//		alert("Succeeded");
-//	} else {
-//		// alert("Failed");
-//	}
+	// if (theRequest.readyState == 4 && theRequest.status == 200) {
+	// // Succeeded
+	// alert("Succeeded");
+	// } else {
+	// // alert("Failed");
+	// }
+}
+
+var TweetMarker = function(latlng, message) {
+	this.latlng = latlng;
+	this.message = message;
+}
+
+TweetMarker.prototype = new GOverlay();
+
+TweetMarker.prototype = {
+	initialize : function(map) {
+		var div = document.createElement("div");
+		div.style.border = "1px solid #ddd";
+		div.style.background = "#ffffff";
+		div.style.position = "absolute";
+		div.style["-border-radius"] = "8px";
+		div.style["-moz-border-radius"] = "8px";		
+		div.style["padding"] = "4px";
+		
+		map.getPane(G_MAP_MAP_PANE).appendChild(div);
+		this.map = map;
+		this.div = div;
+	},
+	remove : function(map) {
+		if(this.div){
+			this.div.parentNode.removeChild(this.div);
+		}
+	},
+	copy : function() {
+		return new TweetMarker(this.latlng,this.message);
+	},
+	redraw : function(force) {
+		if(!force) return;
+		
+		j$div = jQuery(this.div);
+		j$div.hide();
+				
+		var center = this.map.fromLatLngToDivPixel(this.latlng);		
+		var msg = this.message.text;
+		this.div.style.width = msg.length * 20 + "px";
+		this.div.style.height = "20px";
+		this.div.style.left = (center.x - msg.length * 12) + "px";
+		this.div.style.top = center.y + "px";
+		this.div.innerHTML = msg;		
+		
+		j$div.fadeIn();
+	}
 }
