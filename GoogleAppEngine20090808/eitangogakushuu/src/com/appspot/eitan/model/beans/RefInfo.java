951be60fbacec7dc5c723286b5a5bee02baf69a6
masaki.toyoshima <masaki.toyoshima@3035c6ce-779e-11de-91f8-ab615253d9e8>
2009-08-08 06:44:32 +0000

    初期状態は「×（覚えていない）」にする。

--- GoogleAppEngine20090808/eitangogakushuu/src/com/appspot/eitan/model/beans/RefInfo.java
+++ GoogleAppEngine20090808/eitangogakushuu/src/com/appspot/eitan/model/beans/RefInfo.java
@@ -5,22 +5,23 @@ import java.util.Date;
 
 /**
  * �Q�Ə��
+ * 
  * @author Shinji Otsubo
- *
+ * 
  */
-public class RefInfo implements Serializable{
+public class RefInfo implements Serializable {
     private static final long serialVersionUID = -8918135097760836862L;
 
-//    enum EnumExamResult {POOR, AVERAGE, GOOD};   //  �����A���ʁA�ǂ� �̎O�i�K
+    // enum EnumExamResult {POOR, AVERAGE, GOOD}; // �����A���ʁA�ǂ� �̎O�i�K
 
     /** ×をあらわす評価 */
-    public static final int POOR        = 1 ;
+    public static final int POOR = 1;
 
     /** △をあらわす評価 */
-    public static final int AVERAGE     = 2 ;
+    public static final int AVERAGE = 2;
 
     /** ○をあらわす評価 */
-    public static final int GOOD        = 3 ;
+    public static final int GOOD = 3;
 
     /** �Q�Ɖ� */
     public int refCount = 0;
@@ -32,6 +33,7 @@ public class RefInfo implements Serializable{
     public int getRefCount() {
         return refCount;
     }
+
     public void setRefCount(int refCount) {
         this.refCount = refCount;
     }
@@ -39,43 +41,36 @@ public class RefInfo implements Serializable{
     public Date getLastSearch() {
         return lastSearch;
     }
+
     public void setLastSearch(Date lastSearch) {
         this.lastSearch = lastSearch;
     }
+
     public Date getLastExam() {
         return lastExam;
     }
+
     public void setLastExam(Date lastExam) {
         this.lastExam = lastExam;
     }
 
     /**
-     * �Q�ƊK�����C���N�������g����
      * @return
      */
-    public int incrementRefCount(){
-        if(refCount < Integer.MAX_VALUE) ++refCount;    //  �ꉞ�I�[�o�[�t���[�΍�
+    public int incrementRefCount() {
+        if (refCount < Integer.MAX_VALUE)
+            ++refCount; // �ꉞ�I�[�o�[�t���[�΍�
         return refCount;
     }
 
-    public int examResult ;
+    public int examResult = POOR;
 
     public int getExamResult() {
         return examResult;
     }
+
     public void setExamResult(int examResult) {
         this.examResult = examResult;
     }
-
-
-//  /** �������� */
-//  public EnumExamResult examResult = EnumExamResult.AVERAGE;
-//
-//  public EnumExamResult getExamResult() {
-//      return examResult;
-//  }
-//  public void setExamResult(EnumExamResult examResult) {
-//      this.examResult = examResult;
-//  }
 
 }
