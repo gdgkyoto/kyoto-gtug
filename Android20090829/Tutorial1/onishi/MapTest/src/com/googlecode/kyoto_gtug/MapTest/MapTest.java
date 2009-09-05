83320929f3866fc6fa40d1f8f6f19ee788dca59e
ryo611 <ryo611@3035c6ce-779e-11de-91f8-ab615253d9e8>
2009-09-05 03:17:04 +0000

    一応地図が表示できた。

--- Android20090829/Tutorial1/onishi/MapTest/src/com/googlecode/kyoto_gtug/MapTest/MapTest.java
+++ Android20090829/Tutorial1/onishi/MapTest/src/com/googlecode/kyoto_gtug/MapTest/MapTest.java
@@ -1,13 +1,23 @@
 package com.googlecode.kyoto_gtug.MapTest;
 
-import android.app.Activity;
+//import android.app.Activity;
+import com.google.android.maps.*;
 import android.os.Bundle;
 
-public class MapTest extends Activity {
+public class MapTest extends MapActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        MapView map_view = (MapView)findViewById(R.id.mapview);
+        map_view.setBuiltInZoomControls(true);
+    }
+    
+    @Override
+    protected boolean isRouteDisplayed()
+    {
+    	return false;
     }
 }
