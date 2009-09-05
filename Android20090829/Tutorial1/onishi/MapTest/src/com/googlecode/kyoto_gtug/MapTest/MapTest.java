4e7a02005a1e2d240a92ba6eb4d2a596c80ba465
ryo611 <ryo611@3035c6ce-779e-11de-91f8-ab615253d9e8>
2009-09-05 04:18:16 +0000

    ボタンを押したら、特定位置を中心としてマップを表示

--- Android20090829/Tutorial1/onishi/MapTest/src/com/googlecode/kyoto_gtug/MapTest/MapTest.java
+++ Android20090829/Tutorial1/onishi/MapTest/src/com/googlecode/kyoto_gtug/MapTest/MapTest.java
@@ -4,20 +4,76 @@ package com.googlecode.kyoto_gtug.MapTest;
 import com.google.android.maps.*;
 import android.os.Bundle;
 
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+
 public class MapTest extends MapActivity {
+	MapView mapView;
+	Button gpsButton;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        MapView map_view = (MapView)findViewById(R.id.mapview);
-        map_view.setBuiltInZoomControls(true);
+        //MapView map_view = (MapView)findViewById(R.id.mapview);
+        init();
+        
     }
     
     @Override
     protected boolean isRouteDisplayed()
     {
     	return false;
+    }
+    
+    public void init()
+    {
+    	initMap();
+    	initButton();
+    }
+    
+    public void initMap()
+    {
+    	mapView = (MapView)findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true); 	
+    }
+    
+    public void initButton()
+    {
+    	gpsButton = (Button)findViewById(R.id.gpsButton);
+    	GpsButtonClickListener listener = new GpsButtonClickListener( this );
+    	gpsButton.setOnClickListener(listener);
+    }
+    
+    public void setPosInMap( double latitude, double longitude )
+    {
+    	MapController mapCtrl = mapView.getController();
+    	int latitude_1e6 = (int)( latitude * 1E6 );
+    	int longitude_1e6 = (int)( longitude * 1E6 );
+    	GeoPoint point = new GeoPoint( latitude_1e6, longitude_1e6 );
+    	mapCtrl.setCenter( point );
+    	mapCtrl.setZoom( 16 );
+    }
+    
+    public void clickedGpsButton()
+    {
+    	setPosInMap( 34.0d + 59.0d / 60 + 42.0d / 3600,
+   		     		 135.0d + 44.0d / 60 + 19.0d / 3600 );
+    }
+    
+    class GpsButtonClickListener implements OnClickListener
+    {
+    	MapTest target;
+    	public GpsButtonClickListener( MapTest t )
+    	{
+    		target = t;
+    	}
+    	
+    	public void onClick(View v)
+    	{
+    		target.clickedGpsButton();
+    	}
     }
 }
