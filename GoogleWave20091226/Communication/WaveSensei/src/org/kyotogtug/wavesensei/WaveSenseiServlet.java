9be85b1951742d44fd4caac4fe9e0b3c7fd226aa
sonsontomato <sonsontomato@3035c6ce-779e-11de-91f8-ab615253d9e8>
2009-12-26 06:17:48 +0000

    Blip作成されたときにデータを保存するメソッド

--- GoogleWave20091226/Communication/WaveSensei/src/org/kyotogtug/wavesensei/WaveSenseiServlet.java
+++ GoogleWave20091226/Communication/WaveSensei/src/org/kyotogtug/wavesensei/WaveSenseiServlet.java
@@ -1,13 +1,15 @@
 package org.kyotogtug.wavesensei;
 
-import java.io.IOException;
-import javax.servlet.http.*;
+import org.kyotogtug.wavesensei.tw.TwitterSensei;
+
+import com.google.wave.api.AbstractRobotServlet;
+import com.google.wave.api.RobotMessageBundle;
 
 @SuppressWarnings("serial")
-public class WaveSenseiServlet extends HttpServlet {
-  public void doGet(HttpServletRequest req, HttpServletResponse resp)
-      throws IOException {
-    resp.setContentType("text/plain");
-    resp.getWriter().println("Hello, world");
-  }
+public class WaveSenseiServlet extends AbstractRobotServlet {
+	//Blipが作られた時にデータを保存する
+	public void processEvents(RobotMessageBundle bundle) {
+		TwitterSensei twsensei = new TwitterSensei();
+		twsensei.processEvents(bundle);
+	}
 }
