8a75dbf5d9def2973062eaa74ddcbd04f653a1d6
tkuro11 <tkuro11@3035c6ce-779e-11de-91f8-ab615253d9e8>
2009-12-26 04:55:28 +0000

    debug out on

--- GoogleWave20091226/Game/lifegame/lifegame.js
+++ GoogleWave20091226/Game/lifegame/lifegame.js
@@ -29,7 +29,7 @@ function plot_point(e) {
 
 	ary = e.target.id.slice(5).split(',');
 
-	cells[idx(ary[0], ary[1])] = "1";
+	//cells[idx(ary[0], ary[1])] = "1";
 
 	state.submitDelta({'cellstate': cells});
 }
