2b244262ea7efbf0d63547f60a691c62f36bc0b1
bufferings <bufferings@3035c6ce-779e-11de-91f8-ab615253d9e8>
2009-12-26 04:47:28 +0000

    [WaveSensei] TwPostにcreated属性を追加

--- GoogleWave20091226/Communication/WaveSensei/src/org/kyotogtug/wavesensei/tw/model/TwPost.java
+++ GoogleWave20091226/Communication/WaveSensei/src/org/kyotogtug/wavesensei/tw/model/TwPost.java
@@ -1,160 +1,182 @@
 package org.kyotogtug.wavesensei.tw.model;
 
 import java.io.Serializable;
-
-import com.google.appengine.api.datastore.Key;
+import java.util.Date;
 
 import org.slim3.datastore.Attribute;
 import org.slim3.datastore.Model;
 
+import com.google.appengine.api.datastore.Key;
+
 @Model
 public class TwPost implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    @Attribute(primaryKey = true)
-    private Key key;
+  @Attribute(primaryKey = true)
+  private Key key;
 
-    @Attribute(version = true)
-    private Long version;
+  @Attribute(version = true)
+  private Long version;
 
-    private Integer schemaVersion = 1;
-    
-    private String waveId;
-    
-    private String postBlipId;
-    
-    private String creator;
-    
-    private String content;
-    
-    private String postTwitId;
+  private Integer schemaVersion = 1;
 
-    /**
-     * Returns the key.
-     *
-     * @return the key
-     */
-    public Key getKey() {
-        return key;
+  private String waveId;
+
+  private String postBlipId;
+
+  private String creator;
+
+  private String content;
+
+  private String postTwitId;
+
+  private Date created;
+
+  /**
+   * Returns the key.
+   * 
+   * @return the key
+   */
+  public Key getKey() {
+    return key;
+  }
+
+  /**
+   * Sets the key.
+   * 
+   * @param key
+   *          the key
+   */
+  public void setKey(Key key) {
+    this.key = key;
+  }
+
+  /**
+   * Returns the version.
+   * 
+   * @return the version
+   */
+  public Long getVersion() {
+    return version;
+  }
+
+  /**
+   * Sets the version.
+   * 
+   * @param version
+   *          the version
+   */
+  public void setVersion(Long version) {
+    this.version = version;
+  }
+
+  /**
+   * Returns the schema version.
+   * 
+   * @return the schema version
+   */
+  public Integer getSchemaVersion() {
+    return schemaVersion;
+  }
+
+  /**
+   * Sets the schema version.
+   * 
+   * @param schemaVersion
+   *          the schema version
+   */
+  public void setSchemaVersion(Integer schemaVersion) {
+    this.schemaVersion = schemaVersion;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((key == null) ? 0 : key.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
     }
-
-    /**
-     * Sets the key.
-     *
-     * @param key
-     *            the key
-     */
-    public void setKey(Key key) {
-        this.key = key;
+    if (obj == null) {
+      return false;
     }
-
-    /**
-     * Returns the version.
-     *
-     * @return the version
-     */
-    public Long getVersion() {
-        return version;
+    if (getClass() != obj.getClass()) {
+      return false;
     }
-
-    /**
-     * Sets the version.
-     *
-     * @param version
-     *            the version
-     */
-    public void setVersion(Long version) {
-        this.version = version;
+    TwPost other = (TwPost) obj;
+    if (key == null) {
+      if (other.key != null) {
+        return false;
+      }
+    } else if (!key.equals(other.key)) {
+      return false;
     }
+    return true;
+  }
 
-    /**
-     * Returns the schema version.
-     *
-     * @return the schema version
-     */
-    public Integer getSchemaVersion() {
-        return schemaVersion;
-    }
+  public String getWaveId() {
+    return waveId;
+  }
 
-    /**
-     * Sets the schema version.
-     *
-     * @param schemaVersion
-     *            the schema version
-     */
-    public void setSchemaVersion(Integer schemaVersion) {
-        this.schemaVersion = schemaVersion;
-    }
+  public void setWaveId(String waveId) {
+    this.waveId = waveId;
+  }
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((key == null) ? 0 : key.hashCode());
-        return result;
-    }
+  public String getCreator() {
+    return creator;
+  }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        TwPost other = (TwPost) obj;
-        if (key == null) {
-            if (other.key != null) {
-                return false;
-            }
-        } else if (!key.equals(other.key)) {
-            return false;
-        }
-        return true;
-    }
+  public void setCreator(String creator) {
+    this.creator = creator;
+  }
 
-    public String getWaveId() {
-      return waveId;
-    }
+  public String getContent() {
+    return content;
+  }
 
-    public void setWaveId(String waveId) {
-      this.waveId = waveId;
-    }
+  public void setContent(String content) {
+    this.content = content;
+  }
 
+  public String getPostTwitId() {
+    return postTwitId;
+  }
 
-    public String getCreator() {
-      return creator;
-    }
+  public void setPostTwitId(String postTwitId) {
+    this.postTwitId = postTwitId;
+  }
 
-    public void setCreator(String creator) {
-      this.creator = creator;
-    }
+  public String getPostBlipId() {
+    return postBlipId;
+  }
 
-    public String getContent() {
-      return content;
-    }
+  public void setPostBlipId(String postBlipId) {
+    this.postBlipId = postBlipId;
+  }
 
-    public void setContent(String content) {
-      this.content = content;
-    }
+  public Date getCreated() {
+    return created;
+  }
 
-    public String getPostTwitId() {
-      return postTwitId;
-    }
+  public void setCreated(Date created) {
+    this.created = created;
+  }
 
-    public void setPostTwitId(String postTwitId) {
-      this.postTwitId = postTwitId;
-    }
-
-    public String getPostBlipId() {
-      return postBlipId;
-    }
-
-    public void setPostBlipId(String postBlipId) {
-      this.postBlipId = postBlipId;
-    }
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("TwTwit[");
+    sb.append("waveId:").append(waveId).append(", ");
+    sb.append("postBlipId:").append(postBlipId).append(", ");
+    sb.append("creator:").append(creator).append(", ");
+    sb.append("content:").append(content).append(", ");
+    sb.append("created:").append(created).append(", ");
+    sb.append("postTwitId:").append(postTwitId).append("]");
+    return sb.toString();
+  }
 }
