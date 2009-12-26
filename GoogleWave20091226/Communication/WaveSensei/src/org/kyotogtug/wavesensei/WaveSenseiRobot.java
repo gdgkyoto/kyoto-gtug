76abbcfc939128f0e8b67a7a03d97ef8a359f337
yoshinori.ehara <yoshinori.ehara@3035c6ce-779e-11de-91f8-ab615253d9e8>
2009-12-26 04:31:44 +0000

    振り分けサーブレット実装

---
+++ GoogleWave20091226/Communication/WaveSensei/src/org/kyotogtug/wavesensei/WaveSenseiRobot.java
@@ -0,0 +1,98 @@
+package org.kyotogtug.wavesensei;
+
+import java.util.logging.Logger;
+
+import org.kyotogtug.wavesensei.meta.CommonStatusMeta;
+import org.kyotogtug.wavesensei.model.CommonStatus;
+import org.kyotogtug.wavesensei.tutorial.TutorialSensei;
+import org.slim3.datastore.Datastore;
+
+import com.google.wave.api.AbstractRobotServlet;
+import com.google.wave.api.Blip;
+import com.google.wave.api.Event;
+import com.google.wave.api.EventType;
+import com.google.wave.api.Gadget;
+import com.google.wave.api.RobotMessageBundle;
+
+public class WaveSenseiRobot extends AbstractRobotServlet {
+
+    private Logger log = Logger.getLogger(this.getClass().getName());
+    public static final String DEFAULT_SENSEI_TYPE = "0";
+    public static final String SENSEI_GADGET_URL = "http://hosting.gmodules.com/ig/gadgets/file/100410660575232591215/ChooseWS.xml";
+
+    @Override
+    public void processEvents(RobotMessageBundle bundle) {
+        String gSensei = sensei(bundle);
+        if (gSensei.equals("TWITTER")) {
+            // ついった先生
+        } else {
+            new TutorialSensei().execute(bundle);
+        }
+    }
+
+    /**
+     * Wave 別の状態オブジェクトを取得する。
+     * なければ新規作成する。
+     * @param bundle
+     * @return
+     */
+    private CommonStatus getStatus(RobotMessageBundle bundle) {
+        String waveId = bundle.getWavelet().getWaveId();
+        CommonStatusMeta e = CommonStatusMeta.get();
+        CommonStatus status = Datastore.query(e)
+                .filter(e.waveId.equal(waveId))
+                .asSingle();
+        if (status == null) {
+            status = new CommonStatus();
+            status.setWaveId(waveId);
+            status.setTeacherType(DEFAULT_SENSEI_TYPE);
+            saveStatus(status);
+        }
+        return status;
+    }
+
+    private void saveStatus(CommonStatus status) {
+        Datastore.put(status);
+    }
+
+    /**
+     * イベントと永続化情報から、現在の先生タイプを返す。
+     * 先生変更を検知したらその情報を保存しておく
+     * @param bundle
+     * @return 先生タイプ
+     */
+    public String sensei(RobotMessageBundle bundle) {
+        CommonStatus status = getStatus(bundle);
+        String cSenseiType = status.getTeacherType();
+        String gSenseiType = null;
+        for (Event e : bundle.getEvents()) {
+           Gadget g = getSenseiGadget(e.getBlip());
+           if(g == null) continue;
+           gSenseiType = g.getProperty("SenseiType");
+        }
+        if(gSenseiType == null) {
+            return cSenseiType;
+        }
+        if(!gSenseiType.equals(cSenseiType)){
+            status.setTeacherType(gSenseiType);
+            Datastore.put(status);
+        }
+        return gSenseiType;
+    }
+    
+    /**
+     * Blip から先生選択ガジェットを取得する
+     * なければ null
+     * @param blip
+     * @return
+     */
+    private Gadget getSenseiGadget(Blip blip) {
+        for(Gadget g : blip.getDocument().getGadgetView().getGadgets()) {
+            if(g.getUrl().equals(SENSEI_GADGET_URL)) {
+                return g;
+            }
+        }
+        return null;
+    }
+
+}
---
+++ GoogleWave20091226/Communication/WaveSensei/src/org/kyotogtug/wavesensei/model/CommonStatus.java
@@ -0,0 +1,55 @@
+package org.kyotogtug.wavesensei.model;
+
+import org.slim3.datastore.Attribute;
+import org.slim3.datastore.Model;
+
+import com.google.appengine.api.datastore.Key;
+
+@Model
+public class CommonStatus {
+
+    private static final long serialVersionUID = 1L;
+
+    @Attribute(primaryKey = true)
+    private Key key;
+
+    @Attribute(version = true)
+    private Long version;
+    
+    private String waveId;
+    
+    private String teacherType;
+    
+    public Key getKey() {
+        return key;
+    }
+
+    public void setKey(Key key) {
+        this.key = key;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+    public String getWaveId() {
+        return waveId;
+    }
+
+    public void setWaveId(String waveId) {
+        this.waveId = waveId;
+    }
+
+    public String getTeacherType() {
+        return teacherType;
+    }
+
+    public void setTeacherType(String teacherType) {
+        this.teacherType = teacherType;
+    }
+    
+}
