67e254e88380e7c4603213adaf452095b3f5b78f
tkuro11 <tkuro11@3035c6ce-779e-11de-91f8-ab615253d9e8>
2009-12-27 07:03:27 +0000

    sub version test

--- GoogleWave20091226/Game/lifegame/lifesub.js
+++ GoogleWave20091226/Game/lifegame/lifesub.js
@@ -42,7 +42,6 @@ function around_cell(cells, x, y, flag)
 function succ(cells)
 {
 	var outcells = cells;
-	alert(around_cell(cells, 1,1, true));
 	for (var y = 0; y< height; y++) {
 		for (var x = 0; x< width; x++) {
 			var center = cells[idx(x, y)];
