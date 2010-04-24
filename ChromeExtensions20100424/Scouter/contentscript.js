22514cbd921960a364f823299d7fbfd55d9037d0
rich850 <rich850@3035c6ce-779e-11de-91f8-ab615253d9e8>
2010-04-24 05:10:59 +0000

    Raphael使ってSVGで検索エンジン数を表示するようにした

--- ChromeExtensions20100424/Scouter/contentscript.js
+++ ChromeExtensions20100424/Scouter/contentscript.js
@@ -1,5 +1,19 @@
+// SVGの描画にRaphaelというライブラリを使用しています。
+// Raphael Reference: http://raphaeljs.com/reference.html
+var SVG_NS = "http://www.w3.org/2000/svg";
+
 function onResult(result) {
-	alert(result.length + "種類の検索エンジンで検索したよ！");
+	var res = document.getElementById("res");
+	var container = document.createElement("div");
+	container.style.float = "right";
+	res.parentNode.insertBefore(container, res);
+	var paper = Raphael(container, 200, 400);
+	paper.text(100, 20, "Search from " + result.length + " engines.");
+}
+
+function makeSvg(data) {
+	var svg = Raphael(0, 0, 200, 400);
+	return svg;
 }
 
 // background.htmlにmessage passing
