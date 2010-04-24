3e01411e0dc88472ec257fb3fc6557c5c76b7bba
rich850 <rich850@3035c6ce-779e-11de-91f8-ab615253d9e8>
2010-04-24 06:11:33 +0000

    結果をとりあえず文字で表示するようにした

--- ChromeExtensions20100424/Scouter/contentscript.js
+++ ChromeExtensions20100424/Scouter/contentscript.js
@@ -11,11 +11,26 @@ function onResult(result) {
 	var container = document.createElement("div");
 	container.style.float = "right";
 	res.parentNode.insertBefore(container, res);
-	var paper = Raphael(container, 200, 400);
-	paper.text(100, 20, "Search from " + result.length + " engines.");
-	paper.text(100, 40, result[2].data[0].title);
-	paper.text(100, 60, result[2].data[1].title);
-	paper.text(100, 80, result[2].data[2].title);
+	container.innerHTML = dataToHtml(result);
+
+	//var paper = Raphael(container, 200, 400);
+	//paper.text(100, 20, "Search from " + result.length + " engines.");
+	//paper.text(100, 40, result[2].data[0].title);
+	//paper.text(100, 60, result[2].data[1].title);
+	//paper.text(100, 80, result[2].data[2].title);
+}
+
+function dataToHtml(data) {
+	var i, j;
+	var html = "";
+	for (i = 0; i < data.length; i++) {
+		html += data[i].engine + '<ol>';
+		for (j = 0; j < data[i].data.length; j++) {
+			html += '<li style="list-style-type: decimal;">' + data[i].data[j].title + "</li>";
+		}
+		html += "</ol>";
+	}
+	return html;
 }
 
 function makeSvg(data) {
