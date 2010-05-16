81b387bf27942cd6165243a0f0cadc276440606e
hiroki-m <hiroki-m@gaia.eonet.ne.jp>
2010-05-16 05:47:21 +0000

    メッセージと距離を変更

--- HTML520100516/Geo/war/message.js
+++ HTML520100516/Geo/war/message.js
@@ -1,17 +1,19 @@
 function getMessage(distance){
 	if(distance < 1000){
-		return "まだ歩き始めたばかりです。";
+		return "まだ歩き始めたばかりです。頑張ってください。";
+	}else if(distance < 2000){
+		return "1km地点を超えました。まだまだこれからです。";
 	}else if(distance < 5000){
-		return "今、1km以上歩いています。";
+		return "2km地点を超えました。いい運動になったのでは？";
 	}else if(distance < 10000){
-		return "もう5kmを超えました。";
+		return "5km地点を超えました。冷たいお茶でも飲みましょうか。";
 	}else if(distance < 20000){
-		return "現在10km超えです。";
-	}else if(distance < 500000){
-		return "なんと50km超えました!";
-	}else if(distance < 1000000){
-		return "100kmも歩いています!";
+		return "10km地点を超えました。少し休憩しませんか？";
+	}else if(distance < 50000){
+		return "20km地点を超えました。いい運動になりましたね！";
+	}else if(distance < 100000){
+		return "50km地点を超えました。素晴らしい走行距離です!";
 	}else{
-		return "100km以上も歩いていますよ～";
+		return "100km地点を超えました!今日はもう休んでください！";
 	}
 }
