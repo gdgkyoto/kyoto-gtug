ee0a9f677568b356dc2516e05082da4de178fd1c
16777215.likr <16777215.likr@3035c6ce-779e-11de-91f8-ab615253d9e8>
2010-05-16 04:38:19 +0000

    マウスクリックの位置

--- HTML520100516/Network-VNC/html/mouse.js
+++ HTML520100516/Network-VNC/html/mouse.js
@@ -1,15 +1,28 @@
 function onMousePress(eve) {
 	eve.stopPropagation();
 	eve.preventDefault();
-	sendMessage( 'MOUSE_PRESS', 0, getMouseInfo(eve).join(','));
+	sendMessage( 'MOUSE_PRESS', 0, getMouseInfo(eve, this).join(','));
 }
 
 function onMouseRelease(eve) {
 	eve.stopPropagation();
 	eve.preventDefault();
-	sendMessage( 'MOUSE_RELEASE', 0, getMouseInfo(eve).join(','));
+	sendMessage( 'MOUSE_RELEASE', 0, getMouseInfo(eve, this).join(','));
+}
+function onMouseMoveScreen(eve) {
+	eve.stopPropagation();
+	eve.preventDefault();
+	sendMessage("CURSOR_MOVE", 0, getMousePos(eve, this).join(","));
 }
 
-function getMouseInfo(event){
-	return [123, 456, event.button];
+function getMouseInfo(event, target){
+	var info = getMousePos(event, target);
+	info[2] = event.button;
+	return info;
+}
+
+function getMousePos(event, target){
+	var moveX = event.pageX - target.x;
+	var moveY = event.pageY - target.y;
+	return new Array(moveX, moveY);
 }
--- HTML520100516/Network-VNC/html/mouse_move.js
+++ HTML520100516/Network-VNC/html/mouse_move.js
@@ -1,10 +1,10 @@
-function onMouseMoveScreen(eve) {
-	eve.stopPropagation();
-	eve.preventDefault();
-	
-	var moveX = eve.pageX - this.x;
-	var moveY = eve.pageY - this.y;
-	var arr = new Array(moveX, moveY);
-	
-	sendMessage("CURSOR_MOVE", 0, arr.join(","));
-}
+//function onMouseMoveScreen(eve) {
+//	eve.stopPropagation();
+//	eve.preventDefault();
+//	
+//	var moveX = eve.pageX - this.x;
+//	var moveY = eve.pageY - this.y;
+//	var arr = new Array(moveX, moveY);
+//	
+//	sendMessage("CURSOR_MOVE", 0, arr.join(","));
+//}
