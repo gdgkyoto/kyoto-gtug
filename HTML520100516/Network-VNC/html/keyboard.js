e01c3eb40bba5c591724782a51c64e867221e18a
16777215.likr <16777215.likr@3035c6ce-779e-11de-91f8-ab615253d9e8>
2010-05-16 05:54:49 +0000

    特殊キー無効化

--- HTML520100516/Network-VNC/html/keyboard.js
+++ HTML520100516/Network-VNC/html/keyboard.js
@@ -1,8 +1,10 @@
 function onKeyPress(event){
+	event.preventDefault();
 	console.log(event.keyCode);
 	sendMessage('KEY_PRESS', 0, event.keyCode);
 }
 function onKeyRelease(event){
+	event.preventDefault();
 	console.log(event.keyCode);
 	sendMessage('KEY_RELEASE', 0, event.keyCode);
 }
