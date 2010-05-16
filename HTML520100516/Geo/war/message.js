3c312917dd7d8cf7fc001eba0332ee46df41df98
hiroki-m <hiroki-m@gaia.eonet.ne.jp>
2010-05-16 03:06:53 +0000

    メッセージ取得関数

---
+++ HTML520100516/Geo/war/message.js
@@ -0,0 +1,17 @@
+function getMessage(distance){
+	if(distance < 5){
+		return "5km";
+	}else if(distance < 10){
+		return "10km";
+	}else if(distance < 15){
+		return "15km";
+	}else if(distance < 20){
+		return "20km";
+	}else if(distance < 25){
+		return "25km";
+	}else if(distance < 30){
+		return "30km";
+	}else if(distance < 35){
+		return "35km";
+	}
+}
