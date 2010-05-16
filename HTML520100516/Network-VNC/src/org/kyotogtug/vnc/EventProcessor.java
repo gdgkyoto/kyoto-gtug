a469246401edd510602b16b0e343620754974fba
kenjikitamura <kenjikitamura@3035c6ce-779e-11de-91f8-ab615253d9e8>
2010-05-16 03:51:09 +0000

    マウスプレス、リリース実装

--- HTML520100516/Network-VNC/src/org/kyotogtug/vnc/EventProcessor.java
+++ HTML520100516/Network-VNC/src/org/kyotogtug/vnc/EventProcessor.java
@@ -2,10 +2,12 @@ package org.kyotogtug.vnc;
 
 import java.awt.AWTException;
 import java.awt.Robot;
+import java.awt.event.InputEvent;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
+import java.util.logging.Logger;
 
 import org.apache.commons.codec.binary.Base64;
 import org.eclipse.jetty.websocket.WebSocket.Outbound;
@@ -14,6 +16,7 @@ import org.kyotogtug.vnc.events.FileEvent;
 import org.kyotogtug.vnc.events.ImageEvent;
 import org.kyotogtug.vnc.events.ImageRequestEvent;
 import org.kyotogtug.vnc.events.KeyEvent;
+import org.kyotogtug.vnc.events.MousePressEvent;
 import org.kyotogtug.vnc.events.MouseReleaseEvent;
 import org.kyotogtug.vnc.events.MouseMoveEvent;
 import org.kyotogtug.vnc.events.ScreenCapturer;
@@ -22,6 +25,10 @@ public class EventProcessor {
     
     static private Robot robot;
     static private ScreenCapturer capturer;
+    
+    /** get log instance */
+	private static org.apache.commons.logging.Log log = org.apache.commons.logging.LogFactory
+			.getLog(EventProcessor.class);
     
     static {
         try {
@@ -86,17 +93,33 @@ public class EventProcessor {
 	        MouseReleaseEvent e = (MouseReleaseEvent)event;
 	        robot.mouseMove(e.getX(), e.getY());
 	        robot.mouseRelease(toRobotButtonId(e.getButton()));
+	    } else if (event instanceof MousePressEvent ){
+	    	MousePressEvent e = (MousePressEvent)event;
+	        robot.mouseMove(e.getX(), e.getY());
+	        robot.mousePress(toRobotButtonId(e.getButton()));
 	    } else if (event instanceof MouseMoveEvent) {
 	        MouseMoveEvent ev = (MouseMoveEvent)event;
 	        robot.mouseMove(ev.getX(), ev.getY());
 	    }
 	}
+	
 
     private int toRobotButtonId(int button) {
-        switch (button) {
-        default:
-            return button;
-        }
+		switch( button ){
+		case 0: // 左クリック 
+			log.debug("左クリック");
+			return InputEvent.BUTTON1_MASK;
+		case 1: // ホイールクリック
+			log.debug("ホイールクリック");
+			return InputEvent.BUTTON2_MASK;
+		case 2: // 右クリック
+			log.debug("右クリック");
+			return InputEvent.BUTTON3_MASK;
+		default:
+			log.warn("未知のボタンを受信しました。マウスボタン="+button);
+			break;
+		}
+		return -1;
     }
 
 
