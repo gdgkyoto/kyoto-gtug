fb9f4f521937e3065677ea3d7977a43cbdce9af6
16777215.likr <16777215.likr@3035c6ce-779e-11de-91f8-ab615253d9e8>
2010-05-16 01:59:36 +0000

    デバッグ用メッセージ送信

--- HTML520100516/Network-VNC/html/websocket.js
+++ HTML520100516/Network-VNC/html/websocket.js
@@ -1,13 +1,9 @@
 var imageRequestTimer = null;
 var browser;
 var remote;
-var webSocket;
+//var webSocket;
 var receiveIndexes = [];
 var receiveData = {};
-function sendMessage(type, seq, data) {
-//	webSocket.send(data.join(","));
-	webSocket.send([type, seq, new Date().getTime(), data].join('|'));
-}
 function requestImage() {
 	sendMessage("image", 0, '');
 }
@@ -77,87 +73,25 @@ function onMouseWheelImage(eve) {
  * onKeyPressImage(eve){ console.log(eve.charCode); //
  * sendMessage(["keypress"]); }
  */
-function onOpenWebSocket() {
-	browser.addEventListener("mousemove", onMouseMoveBrowser, false);
-	remote.addEventListener("mousedown", onMouseDownImage, false);
-	remote.addEventListener("mouseup", onMouseUpImage, false);
-	remote.addEventListener("mousewheel", onMouseWheelImage, false);
-	// window.addEventListener("keydown",onKeyDownImage,false);
-	// window.addEventListener("keyup",onKeyUpImage,false);
-	// window.addEventListener("keypress",onKeyPressImage,false);
-	requestImage();
-}
-function onCloseWebSocket() {
-}
-function onMessageWebSocket(message) {
-	var data = message.data;
-	var position = data.indexOf("_");
-	var messageType;
-	var messageData;
-	if (position == -1) {
-		messageType = data;
-		messageData = "";
-	} else {
-		messageType = data.slice(0, position);
-		messageData = data.slice(position + 1);
-	}
-	switch (messageType) {
-	case "image":
-		onMessageWebSocketImage(messageData);
-		break;
-	}
-}
-function onMessageWebSocketImage(data) {
-	setImageRequestTimer(3000);
-	var splitData = data.split("_");
-	if (splitData.length != 4) {
-		return;
-	}
-	var capturedDate = splitData[0];
-	var sequenceNumber = parseInt(splitData[1], 16);
-	var sequenceCount = parseInt(splitData[2], 16);
-	if (receiveIndexes.indexOf(capturedDate) == -1) {
-		receiveIndexes.push(capturedDate);
-		receiveIndexes.sort(function(a, b) {
-			return parseInt(a, 16) - parseInt(b, 16);
-		});
-		receiveData[capturedDate] = {
-			"data" : [],
-			"receiveCount" : 0,
-			"sequenceCount" : sequenceCount
-		};
-	}
-	var receive = receiveData[capturedDate];
-	receive.receiveCount++;
-	receive.data[sequenceNumber] = splitData[3];
-	if (receive.receiveCount != receive.sequenceCount) {
-		return;
-	}
-	var currentPosition = receiveIndexes.indexOf(capturedDate);
-	deleteIndexes = receiveIndexes.slice(0, currentPosition + 1);
-	receiveIndexes = receiveIndexes.slice(currentPosition + 2);
-	for ( var i = 0; i < deleteIndexes.length; i++) {
-		delete receiveData[deleteIndexes[i]];
-	}
-	var base64 = "";
-	for ( var i = 1; i < receive.data.length; i++) {
-		base64 += receive.data[i];
-	}
-	remote.setAttribute("src", "data:image/jpg;base64," + base64);
-	setImageRequestTimer(100);
-}
-function onUnloadWindow() {
-	webSocket.close();
-}
+//function onOpenWebSocket() {
+//	browser.addEventListener("mousemove", onMouseMoveBrowser, false);
+//	remote.addEventListener("mousedown", onMouseDownImage, false);
+//	remote.addEventListener("mouseup", onMouseUpImage, false);
+//	remote.addEventListener("mousewheel", onMouseWheelImage, false);
+//	// window.addEventListener("keydown",onKeyDownImage,false);
+//	// window.addEventListener("keyup",onKeyUpImage,false);
+//	// window.addEventListener("keypress",onKeyPressImage,false);
+//	requestImage();
+//}
 function initial(eve) {
-	browser = document.documentElement;
-	remote = document.getElementById("remote");
-	var protocol = (location.protocol == "https:") ? "wss" : "ws";
-	var host = location.host;
-	webSocket = new WebSocket(protocol + "://" + host + "/ws/");
-	webSocket.addEventListener("open", onOpenWebSocket, false);
-	webSocket.addEventListener("close", onCloseWebSocket, false);
-	webSocket.addEventListener("message", onMessageWebSocket, false);
+//	browser = document.documentElement;
+//	remote = document.getElementById("remote");
+//	var protocol = (location.protocol == "https:") ? "wss" : "ws";
+//	var host = location.host;
+//	webSocket = new WebSocket(protocol + "://" + host + "/ws/");
+//	webSocket.addEventListener("open", onOpenWebSocket, false);
+//	webSocket.addEventListener("close", onCloseWebSocket, false);
+//	webSocket.addEventListener("message", onMessageWebSocket, false);
 	window.addEventListener("unload", onUnloadWindow, false);
 	browser.addEventListener("contextmenu", onContextMenuBrowser, false);
 	browser.addEventListener("contextmenu", onContextMenuBrowser, true);
