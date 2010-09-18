6113b3a5e569fb911d3805cf47e6e4075ae1aaad
baba.ayako.baba <baba.ayako.baba@gmail.com>
2010-09-18 07:04:08 +0000

    commit voice recognition activity

---
+++ Android_maps2/VoiceMap2/src/org/hackathon/map2/VoiceRecognitionActivity.java
@@ -0,0 +1,77 @@
+package org.hackathon.map2;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class VoiceRecognitionActivity extends Activity {
+
+	private static final int REQUEST_CODE = 0;
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.voice);
+
+		Intent intent=getIntent();
+		String lat=intent.getStringExtra("lat");
+		String lon = intent.getStringExtra("lon");
+		
+		Toast.makeText(VoiceRecognitionActivity.this, lat+":"+lon, Toast.LENGTH_LONG);
+
+		Button button = (Button) findViewById(R.id.Button01);
+
+		button.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				try {
+
+					Intent intent = new Intent(
+							RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+					intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+							RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+					intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+							"VoiceRecognition");
+					startActivityForResult(intent, REQUEST_CODE);
+				} catch (ActivityNotFoundException e) {
+					Toast.makeText(VoiceRecognitionActivity.this,
+							"ActivityNotFoundException", Toast.LENGTH_LONG)
+							.show();
+				}
+
+			}
+		});
+
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+		if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+			String resultsString = "";
+
+			List<String> results = data
+					.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+			for (int i = 0; i < results.size(); i++) {
+				resultsString += results.get(i);
+			}
+
+			Intent intent = new Intent();
+			
+			
+		}
+
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+
+}
