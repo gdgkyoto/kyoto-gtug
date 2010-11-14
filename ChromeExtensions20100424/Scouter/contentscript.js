260c41508d11edefc6890fc3e9398cc1f273d135
kabakiyo <kabakiyo@3035c6ce-779e-11de-91f8-ab615253d9e8>
2010-11-14 13:18:26 +0000

    Googleさんのタグが変わったので対応

--- ChromeExtensions20100424/Scouter/contentscript.js
+++ ChromeExtensions20100424/Scouter/contentscript.js
@@ -112,6 +112,6 @@ chrome.extension.sendRequest({
 		console.log(response);
 		if (response.value != 'true')
 			return;
-		showPageRank('li.g > h3 > a');
+		showPageRank('span.tl > h3 > a');
 	}
 );
