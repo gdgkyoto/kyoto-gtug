59ca3862294c3f1e24df8a2bdcfff240d0222e68
Y.A.Unix <Y.A.Unix@gmail.com>
2011-02-19 01:35:23 +0000

    Project file

---
+++ MapsHackath​on20110219/originalMap/HelloBingMaps/HelloBingMaps/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Maps.MapControl.Core;
+using Microsoft.Maps.MapControl;
+
+namespace HelloBingMaps
+{
+    public partial class MainPage : UserControl
+    {
+        private bool drawingLineMode = false;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void changeToMercatorlModeButton_Click(object sender, RoutedEventArgs e)
+        {
+            mainMap.Mode = new MercatorMode();
+        }
+
+        private void changeToAerialModeButton_Click(object sender, RoutedEventArgs e)
+        {
+            mainMap.Mode = new AerialMode();
+        }
+
+        private void drawALineButton_Click(object sender, RoutedEventArgs e)
+        {
+            drawingLineMode = true;
+        }
+
+        private void mainMap_MouseClick(object sender, MapMouseEventArgs e)
+        {
+            if (drawingLineMode) {
+            }
+        }
+
+    }
+}
