ad957b5ccc054e845dadd10088cdd8667ebc51c1
fu.shiroyama <fu.shiroyama@gmail.com>
2011-02-19 07:20:04 +0000

    modified

--- MapsHackath​on20110219/originalMap/HelloBingMaps/HelloBingMaps/MapModes/MapImageMode.cs
+++ MapsHackath​on20110219/originalMap/HelloBingMaps/HelloBingMaps/MapModes/MapImageMode.cs
@@ -8,11 +8,47 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using Microsoft.Maps.MapControl;
+using System.Diagnostics;
 
 namespace HelloBingMaps.MapModes
 {
     public class MapImageMode : MapModeImplementation
     {
+
+        private URLWindow window = new URLWindow();
+        private MainPage mainPage;
+        private MapMouseEventArgs mouseEvent; 
+
+        public MapImageMode()
+        {
+            window.Closed += new EventHandler(window_Closed);
+        }
+        
+        void window_Closed(object sender, EventArgs e)
+        {
+            String URL = window.getURL();
+            Debug.WriteLine("URL: " + URL);
+
+            Location location = new Location();
+
+            if (this.mainPage.mainMap.TryViewportPointToLocation(this.mouseEvent.ViewportPoint, out location))
+            {
+                /* Success */
+                Debug.WriteLine("Point (" + location.Longitude + "," + location.Latitude + ")");
+
+                MapImage image = new MapImage(URL);
+                Debug.WriteLine("yabai: " + URL);
+                image.setLocation(location);
+                image.draw(mainPage.mainMap);
+            }
+            else
+            {
+                /* Fails */
+                Debug.WriteLine("Something wrong has happened in converting viewport to location.");
+            }
+        }
+        
         public DrawingMode getDrawingMode()
         {
             return DrawingMode.Image;
@@ -20,22 +56,27 @@ namespace HelloBingMaps.MapModes
 
         public void start(MainPage mainPage)
         {
-            throw new NotImplementedException();
+            // do nothing
         }
 
         public MapData end(MainPage mainPage)
         {
-            throw new NotImplementedException();
+            // do nothing
+            return null;
         }
 
-        public void onClick(MainPage mainPage, object sender, Microsoft.Maps.MapControl.MapMouseEventArgs e)
+        
+
+        public void onClick(MainPage mainPage, object sender, MapMouseEventArgs e)
         {
-            throw new NotImplementedException();
+            this.mainPage = mainPage;
+            this.mouseEvent = e;
+            window.Show(); 
         }
 
         public void onMouseMove(MainPage mainPage, object sender, MouseEventArgs e)
         {
-            throw new NotImplementedException();
+            // do nothing
         }
     }
 }
